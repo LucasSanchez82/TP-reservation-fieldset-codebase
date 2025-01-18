9c3609e55ae90d6f9556d64a4149cde1c5566ab2
LucasSanchez82 <jiraya1008@gmail.com>
2025-01-18 15:32:29 +0100

    feat: add front box

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
+const select = document.querySelector("#select");
+const addButton = document.getElementById("add");
+const form = document.querySelector("form");
+const fieldsetValues = [];
+const addFieldSet = (date, nbPlaces) => {
+  const id = fieldsetValues.length;
+  const fieldset = document.createElement("fieldset");
+  fieldset.classList.add("fieldset");
+  fieldset.id = `fieldset-${id}`;
+  fieldset.innerHTML = `
+  <legend>Réservation</legend>
 
+    <div class="form-group row">
+        <div class="col-sm-4">
+            <label> Date : ${date}</label>
+        </div>
+    </div>
 
+    <div class="form-group row">
+        <div class="col-sm-4">
+        <label> Nombres de places : ${nbPlaces}</label>
+    </div>
+`;
+  fieldsetValues.push({
+    date,
+    nbPlaces,
+    id,
+    ref: fieldset,
+  });
+  form.insertBefore(fieldset, form[0]);
+};
 
-let datesToDisplay = [
-    {value:"2024-01-06"},
-    {value:"2024-01-13"},
-    {value:"2024-01-20"},
-    {value:"2024-01-27"},
-]
+const datesToDisplay = [
+  { value: "2024-01-06" },
+  { value: "2024-01-13" },
+  { value: "2024-01-20" },
+  { value: "2024-01-27" },
+];
 
+datesToDisplay.forEach((date) => {
+  const option = document.createElement("option");
+  [option.value, option.text] = [date.value, date.value];
+  select.appendChild(option);
+});
 
+addButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  const formdata = new FormData(form);
+  const { date, nbPlaces } = Object.fromEntries(formdata);
+  if (!(date && nbPlaces)) {
+    alert("Veuillez remplir tous les champs");
+    throw new Error("Veuillez remplir tous les champs");
+  }
+  addFieldSet(date, nbPlaces);
+});
 
+// fieldset element
+// document.
